0806ff3e1fbbe6605fe06f6d343f9ba922f6653e
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-01 15:52:08 -0700

    fixed all the bugs and can show graph now

--- factor_engines/load_data.py
+++ factor_engines/load_data.py
@@ -3,10 +3,23 @@ import pandas as pd
 
 def get_price_data(tickers, start, end):
     data = yf.download(tickers, start = start, end = end, progress = False, auto_adjust = True)
-    data = data["close"]
 
     data = data.sort_index()
     data = data.ffill().bfill()
+
+    # if isinstance(data.columns, pd.MultiIndex) :
+    #     if "Close" in data.columns.get_level_values(0):
+    #         prices = data.xs("Close", level = "Price")
+    #     else:
+    #         raise KeyError("Could not find 'Close' in MultiIndex columns.")
+    # else:
+    #     if "Close" in data.columns:
+    #         prices = data["Close"]
+    #     else:
+    #         raise KeyError("Could not find 'Close' in columns.")
+
+    if isinstance(data, pd.Series):
+        data = data.to_frame()
 
     return data
 
