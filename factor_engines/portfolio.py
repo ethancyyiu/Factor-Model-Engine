9acaab035ec1c3fb19c15c68b810de237a965084
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-01 16:35:09 -0700

    added weighting to the ranks

--- factor_engines/portfolio.py
+++ factor_engines/portfolio.py
@@ -4,3 +4,14 @@ import numpy as np
 def rank_factor(factors):
     return factor.rank(axis = 1, method = "first")
 
+def long_short_weights(ranks, long_pct=0.2, short_pct=0.2):
+    n_stocks = ranks.shape[1]
+    long_cutoff = (1 - long_pct) * n_stocks
+    short_cutoff = short_pct * n_stocks
+
+    weights = pd.DataFrame(0.0, index=ranks.index, columns=ranks.columns)
+    weights[ranks > long_cutoff] = 1.0
+    weights[ranks <= short_cutoff] = -1.0
+
+    return weights
+    
