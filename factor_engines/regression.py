ba4da9aa56be20618b159fa2bab66d3d2c2112c9
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-03 12:55:48 -0700

    cleaned data

--- factor_engines/regression.py
+++ factor_engines/regression.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+
+def run_ols(y, X, add_constant=True):
+    data = pd.concat([y, X], axis=1).dropna()
+    y_clean = data.iloc[:, 0]
+    X_clean = data.iloc[:, 1:]
+
+    if add_constant:
+        X_clean = sm.add_constant(X_clean)
+
+    model = sm.OLS(y_clean, X_clean)
+    results = model.fit()
+
+    return results
