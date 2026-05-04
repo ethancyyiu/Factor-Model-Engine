19b3b8475ddd474392766af0a355fe42a78af308
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-03 22:49:35 -0700

    trying to combine everything

--- factor_engines/regression.py
+++ factor_engines/regression.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 
-def run_ols(y, X, add_constant=True):
-    data = pd.concat([y, X], axis=1).dropna()
+def run_ols(y, x, add_constant=True):
+    data = pd.concat([y, x], axis=1).dropna()
     y_clean = data.iloc[:, 0]
     X_clean = data.iloc[:, 1:]
 
@@ -20,3 +20,6 @@ def annualize_alpha(alpha_daily, periods_per_year=252):
 
 def annualize_vol(vol_daily, periods_per_year=252):
     return vol_daily * np.sqrt(periods_per_year)
+
+def summerize_regression(y, x, date = 252):
+    result = run_ols(y, x)
